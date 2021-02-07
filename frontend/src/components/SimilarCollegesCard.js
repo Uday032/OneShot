5a6c348b519b6b5bc6740ecd2936859a868b6642
Uday032 <rahul.uday07@gmail.com>
2021-02-08 00:32:41 +0530

    Readme UPdated

---
+++ frontend/src/components/SimilarCollegesCard.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+import {Card} from 'react-bootstrap'
+import DataDetailsList from './DataDetailsList'
+
+export default class SimilarCollegesCard extends Component {
+    render() {
+        return (
+            <Card border="dark">
+                <Card.Body>
+                <Card.Title>{this.props.collegename}</Card.Title>
+                <Card.Text>
+                    <ul>
+                        <DataDetailsList label="Year Founded" value={this.props.collegeyearfounded} />
+                        <DataDetailsList label="No. of Students" value={this.props.collegenoofstudents} />
+                        <DataDetailsList label="Location" value={this.props.collegecity+", "+ this.props.collegestate+", "+ this.props.collegecountry} />
+                        <DataDetailsList label="Courses Provided" value={this.props.coursesoffered} />
+
+                    </ul>
+                    
+                </Card.Text>
+                </Card.Body>
+            </Card>
+        )
+    }
+}
