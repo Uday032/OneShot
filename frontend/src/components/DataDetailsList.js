71d7a08987747157cb7c9d095761065279e39a0d
Uday032 <rahul.uday07@gmail.com>
2021-02-08 00:48:50 +0530

    Final Commit

--- frontend/src/components/DataDetailsList.js
+++ frontend/src/components/DataDetailsList.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
-
+import {Button} from 'react-bootstrap'
 export default class DataDetailsList extends Component {
+    
     render() {
+        let value = this.props.value;
+        let buttons;
+        if(this.props.type === "array"){
+            buttons = this.props.value.map((array)=> {
+                return (
+                    <Button variant="light" className="p-1 m-1 buttonclass">{array}</Button>
+                );
+            })
+            value = buttons
+        }
         return (
             <li>
                 <div>
-                    <span className="h6">{this.props.label}:</span> {this.props.value}
+                    <span className="h6">{this.props.label}:</span> {value}
                 </div>
             </li>
         )
--- frontend/src/components/TableDataStudent.js
+++ frontend/src/components/TableDataStudent.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import {Button} from 'react-bootstrap'
 
 export default class TableDataStudent extends Component {
     render() {
         return (
             <tr>
                 <td>{this.props.Name}</td>
-                <td>{this.props.Skills}</td>
+                <td>
+                    {this.props.Skills.map((skill)=>{
+                        return (
+                            <Button variant="light" className="p-1 m-1 buttonclass">{skill}</Button>
+                        );
+                    })}
+                </td>
                 <td>{this.props.BatchYear}</td>
             </tr>
         )
